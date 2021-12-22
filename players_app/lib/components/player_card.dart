3d62f4a17c20d8788f19ce52ca5d7adb72ae6595
niamulhasan <niamulhasanbd@gmail.com>
2021-12-22 11:57:29 +0600

    player app uncomplete

---
+++ players_app/lib/components/player_card.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class MyPlayerCard extends StatelessWidget {
+  final String image;
+  final String name;
+  const MyPlayerCard({
+    Key? key,
+    required this.image,
+    required this.name,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Padding(
+          padding: const EdgeInsets.fromLTRB(16.0, 28.0, 16.0, 16.0),
+          child: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                alignment: Alignment.topCenter,
+                image: AssetImage("assets/images/card_bg.png"),
+              ),
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(16.0),
+              boxShadow: [
+                BoxShadow(
+                  color: Color(0xff661EFF).withOpacity(0.5),
+                  blurRadius: 5,
+                )
+              ],
+            ),
+            child: Container(
+              width: double.infinity,
+              height: double.infinity,
+              child: Padding(
+                padding: const EdgeInsets.only(
+                  top: 16.0,
+                  left: 12.0,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "$name",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 22.0,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+        Container(
+          width: double.infinity,
+          height: double.infinity,
+          alignment: Alignment.topRight,
+          child: Padding(
+            padding: const EdgeInsets.only(bottom: 28.0),
+            child: Image.asset("assets/images/players/$image"),
+          ),
+        )
+      ],
+    );
+  }
+}
--- players_app/lib/main.dart
+++ players_app/lib/main.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/material.dart';
 
+import 'components/player_card.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  final List<Map<String, String>> playersData = [
+    {"name": "Al Amin", "photo": "alamin.png"},
+    {"name": "Mehide", "photo": "mehide.png"},
+    {"name": "Mustafiz", "photo": "mustafiz.png"},
+    {"name": "Sakib", "photo": "sakib.png"},
+    {"name": "Taijul", "photo": "taijul.png"},
+    {"name": "Taskin", "photo": "taskin.png"},
+    {"name": "Yasir Ali", "photo": "yasir_ali.png"}
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -10,35 +22,22 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: Color(0xff5C24FC),
-          title: Text('Material App Bar'),
+          title: Text('Cricket Player\'s Directory'),
         ),
         body: Container(
           color: Color(0xffF4F5F7),
           height: double.infinity,
           width: double.infinity,
-          child: GridView.count(
-            crossAxisCount: 2,
-            childAspectRatio: (400 / 505),
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                      image: DecorationImage(
-                        alignment: Alignment.topCenter,
-                        image: AssetImage("assets/images/card_bg.png"),
-                      ),
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(16.0),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Color(0xff661EFF).withOpacity(0.5),
-                          blurRadius: 5,
-                        )
-                      ]),
-                ),
-              ),
-            ],
+          child: GridView.builder(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              childAspectRatio: (400 / 505),
+            ),
+            itemCount: playersData.length,
+            itemBuilder: (BuildContext context, int i) => MyPlayerCard(
+              image: playersData[i]["photo"]!,
+              name: playersData[i]["name"]!,
+            ),
           ),
         ),
       ),
