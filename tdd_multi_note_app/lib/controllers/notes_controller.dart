a8c432671b004078bdfebf1e17c5408b38b0835c
niamulhasan <niamulhasanbd@gmail.com>
2022-01-23 16:28:51 +0600

    tdd note

--- tdd_multi_note_app/lib/controllers/notes_controller.dart
+++ tdd_multi_note_app/lib/controllers/notes_controller.dart
@@ -5,4 +5,17 @@ class NotesController {
     SharedPreferences handle = await SharedPreferences.getInstance();
     return handle.getStringList("my_notes");
   }
+
+  Future<List> setNote(String note) async {
+    SharedPreferences handle = await SharedPreferences.getInstance();
+    List<String>? current_notes = handle.getStringList("my_notes");
+    if (current_notes!.isEmpty) {
+      return await handle.setStringList("my_notes", [note]) ? [note] : [];
+    } else {
+      current_notes.add(note);
+      return await handle.setStringList("my_notes", current_notes)
+          ? current_notes
+          : [];
+    }
+  }
 }
--- tdd_multi_note_app/test/contollers/notes_controller_test.dart
+++ tdd_multi_note_app/test/contollers/notes_controller_test.dart
@@ -5,17 +5,44 @@ import 'package:tdd_multi_note_app/controllers/notes_controller.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test("getNotes: ", () async {
-    //Arrange
-    //Phone memory
+  setUp(() {
     SharedPreferences.setMockInitialValues({
-      "my_notes": ["note 1", "note dsafdsaf"]
+      "my_notes": ["note 1", "note"]
     });
+  });
+
+  test("getNotes: shoud return true", () async {
+    //Arrange
 
     //Act
     List<String>? notes = await NotesController().getNotes();
 
     //Assert
     expect(notes, isA<List<String>>());
+  });
+
+  group("setNotes: Group", () {
+    test("setNotes: shoud return a List<String>", () async {
+      //Arrange
+
+      //Act
+      List<dynamic> notesWeGot =
+          await NotesController().setNote("another note");
+
+      //Assert
+      expect(notesWeGot, ["note 1", "note", "another note"]);
+    });
+
+    test("setNotes: should return a List<String> with one item only", () async {
+      //Arrange
+      SharedPreferences.setMockInitialValues({"my_notes": []});
+
+      //Act
+      List<dynamic> notesWeGot =
+          await NotesController().setNote("another note");
+
+      //Assert
+      expect(notesWeGot, ["another note"]);
+    });
   });
 }
